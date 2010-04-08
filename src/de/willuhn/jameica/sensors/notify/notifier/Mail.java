42f1193267ef53d4a4da3bb94d78bbb8b0619f74
willuhn <willuhn>
2010-04-08 12:33:58 +0000

    @B Fehler in SMTP-Authentifizierung

--- src/de/willuhn/jameica/sensors/notify/notifier/Mail.java
+++ src/de/willuhn/jameica/sensors/notify/notifier/Mail.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica.sensors/src/de/willuhn/jameica/sensors/notify/notifier/Mail.java,v $
- * $Revision: 1.6 $
- * $Date: 2010/04/08 11:53:32 $
+ * $Revision: 1.7 $
+ * $Date: 2010/04/08 12:33:58 $
  * $Author: willuhn $
  *
  * Copyright (c) by willuhn - software & services
@@ -16,8 +16,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.Message.RecipientType;
@@ -63,74 +61,92 @@ public class Mail implements Notifier
     if (params == null) // erspart uns unnoetige NULL-Checks
       params = new HashMap<String,String>();
 
-    Properties props = System.getProperties();
-
-    ////////////////////////////////////////////////////////////////////////////
-    // Authentifizierung
-    Authenticator auth = null;
-    final String user = params.get("smtp.username");
-    final String pw   = params.get("smtp.password");
-    if (user != null && pw != null && user.length() > 0 && pw.length() > 0)
+    Transport transport = null;
+    
+    try
     {
-      auth = new Authenticator() {
-        protected PasswordAuthentication getPasswordAuthentication() {
-          return new PasswordAuthentication(user,pw);
+      Properties props = System.getProperties();
+
+      ////////////////////////////////////////////////////////////////////////////
+      // SMTP-Host
+      String host = params.get("smtp.host");
+      props.put("mail.smtp.host",host != null && host.length() > 0 ? host : "localhost");
+      //
+      ////////////////////////////////////////////////////////////////////////////
+
+      ////////////////////////////////////////////////////////////////////////////
+      // Zeichensatz
+      String charset = params.get("mail.charset");
+      props.put("mail.mime.charset",charset != null ? charset : "iso-8859-15");
+      //
+      ////////////////////////////////////////////////////////////////////////////
+
+      // Session erzeugen
+      Session session = Session.getDefaultInstance(props);
+      transport = session.getTransport("smtp");
+
+      
+      ////////////////////////////////////////////////////////////////////////////
+      // Authentifizierung
+      String user = params.get("smtp.username");
+      String pw   = params.get("smtp.password");
+      if (user != null && pw != null && user.length() > 0 && pw.length() > 0)
+      {
+        props.put("mail.smtp.auth", "true");
+        transport.connect(user,pw);
+      }
+      //
+      ////////////////////////////////////////////////////////////////////////////
+
+      
+      ////////////////////////////////////////////////////////////////////////////
+      // Message erzeugen
+      MimeMessage mime = new MimeMessage(session);
+      mime.setSubject(subject);
+      mime.setText(description);
+      mime.setSentDate(new Date());
+      
+      // Absender
+      String from = params.get("mail.sender");
+      if (from != null && from.length() > 0)
+        mime.setFrom(new InternetAddress(from));
+
+      // Empfaenger
+      String recipients = params.get("mail.recipients");
+      if (recipients == null || recipients.length() == 0)
+        throw new Exception("no recipient(s) given. please add param 'mail.recipients' to you notify rule");
+      
+      String[] rl = recipients.split("[,; ]");
+      for (String r:rl)
+      {
+        r = r.trim();
+        if (r.length() == 0)
+          continue;
+        mime.addRecipient(RecipientType.TO,new InternetAddress(r));
+      }
+      //
+      ////////////////////////////////////////////////////////////////////////////
+
+      
+      // Mail senden
+      Logger.info("sending mail to " + recipients);
+      transport.sendMessage(mime,mime.getAllRecipients());
+      Logger.info("message sent");
+    }
+    finally
+    {
+      if (transport != null)
+      {
+        try
+        {
+          transport.close();
         }
-      };
-      props.put("mail.smtp.auth", "true");
+        catch (Exception e)
+        {
+          Logger.error("unable to close smtp transport",e);
+        }
+      }
     }
-    //
-    ////////////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////////////////////////////////////////////////
-    // SMTP-Host
-    String host = params.get("smtp.host");
-    props.put("mail.smtp.host",host != null && host.length() > 0 ? host : "localhost");
-    //
-    ////////////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////////////////////////////////////////////////
-    // Zeichensatz
-    String charset = params.get("mail.charset");
-    System.setProperty("mail.mime.charset",charset != null ? charset : "iso-8859-15");
-    //
-    ////////////////////////////////////////////////////////////////////////////
-    
-
-    Session session = Session.getDefaultInstance(props,auth);
-    MimeMessage mime = new MimeMessage(session);
-    mime.setSubject(subject);
-    mime.setText(description);
-    mime.setSentDate(new Date());
-    
-    ////////////////////////////////////////////////////////////////////////////
-    // Absender
-    String from = params.get("mail.sender");
-    if (from != null && from.length() > 0)
-      mime.setFrom(new InternetAddress(from));
-    //
-    ////////////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////////////////////////////////////////////////
-    // Empfaenger
-    String recipients = params.get("mail.recipients");
-    if (recipients == null || recipients.length() == 0)
-      throw new Exception("no recipient(s) given. please add param 'mail.recipients' to you notify rule");
-    
-    String[] rl = recipients.split("[,; ]");
-    for (String r:rl)
-    {
-      r = r.trim();
-      if (r.length() == 0)
-        continue;
-      mime.addRecipient(RecipientType.TO,new InternetAddress(r));
-    }
-    //
-    ////////////////////////////////////////////////////////////////////////////
-
-    Logger.info("sending mail to " + recipients);
-    Transport.send(mime);
-    Logger.info("message sent");
   }
 }
 
@@ -138,6 +154,9 @@ public class Mail implements Notifier
 
 /**********************************************************************
  * $Log: Mail.java,v $
+ * Revision 1.7  2010/04/08 12:33:58  willuhn
+ * @B Fehler in SMTP-Authentifizierung
+ *
  * Revision 1.6  2010/04/08 11:53:32  willuhn
  * @B Aktivierung der SMTP-Authentifizierung fehlte
  *
